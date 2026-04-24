1bbc2d3df8fad0305637e931a6024903cba082d4
Chen Enquan <0122herbie@gmail.com>
2026-04-24 21:54:45 +0800

    Add: jump-game - submission-7

---
+++ Data Structures & Algorithms/jump-game/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        path = [0] * n
+        i = 0
+        while i >= 0:
+            print(i)
+            if i == (n-1):
+                return True
+            if nums[i] != 0 and not path[i]:
+                path[i] = 1
+                i = min(i+nums[i], n-1)
+            else:
+                path[i] = 1
+                i -= 1
+
+        return False
